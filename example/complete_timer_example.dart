3aee86dde9f722d10973c9d8c1454480adf6ff2d
unknown <zamani3509@gmail.com>
2022-07-02 19:32:05 +0430

    Update example

--- example/complete_timer_example.dart
+++ example/complete_timer_example.dart
@@ -5,23 +5,30 @@ import 'package:complete_timer/complete_timer.dart';
 void normalTimerExample() {
   print('start normal timer');
 
-  // Creates a timer which fires callback after the 5seconds
   final CompleteTimer normalTimer = CompleteTimer(
+    // must a non-negative Duration.
     duration: Duration(seconds: 5),
-    // Defaults to false
+
+    // If periodic sets true
+    // The callback is invoked repeatedly with duration intervals until
+    // canceled with the cancel function.
+    // Defaults to false.
     periodic: false,
-    // Defaults to true
+
+    // If autoStart sets true timer starts automatically, default to true.
     autoStart: true,
+
+    // The callback function is invoked after the given duration.
     callback: (timer) {
       print('normal example');
       timer.stop();
-      // We call stop function before getting elapsed time to get a exact time
+      // We call stop function before getting elapsed time to get a exact time.
       print('normal timer finished after: ${timer.elapsed}');
     },
   );
 
   // After 2 seconds we stop timer and 2 second later we resume it
-  Timer(Duration(seconds: 2), () {
+  Future.delayed(Duration(seconds: 2), () {
     print('stopped normal timer');
     normalTimer.stop();
   });
@@ -37,10 +44,14 @@ void periodic(List<String> args) {
 
   // Creates a periodic timer which fires callback every seconds
   final CompleteTimer periodicTimer = CompleteTimer(
+    // must a non-negative Duration.
     duration: Duration(seconds: 1),
-    // Defaults to false
+    // If periodic sets true
+    // The callback is invoked repeatedly with duration intervals until
+    // canceled with the cancel function.
+    // Defaults to false.
     periodic: true,
-    // Defaults to true
+    // If autoStart sets true timer starts automatically, default to true.
     autoStart: true,
     callback: (timer) {
       print('periodic example');
